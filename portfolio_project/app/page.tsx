031b53e600b09cc422551d1ff18f2c3295633150
Houdini Papi <ochiengcliff.co@gmail.com>
2024-05-31 19:16:36 +0300

    feat: Add Grid component for displaying content in a grid layout

--- portfolio_project/app/page.tsx
+++ portfolio_project/app/page.tsx
@@ -1,3 +1,4 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
@@ -15,6 +16,7 @@ export default function Home() {
           }
         ]} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
