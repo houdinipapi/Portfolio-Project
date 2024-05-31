4c429bbddd95d1c9a690851902538300d41cf039
Houdini Papi <ochiengcliff.co@gmail.com>
2024-05-31 19:38:28 +0300

    Add SVG icons for host, arrow, app, tail, s, and link

--- portfolio_project/components/Grid.tsx
+++ portfolio_project/components/Grid.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
     <section id='about'>
         <BentoGrid>
-            {[
-                {
-                    title: "Title1",
-                    description: "Desc1",
-                    id: 1
-                }
-            ].map
-             ((item, i) => (
+            {gridItems.map
+             ((item) => (
                 <BentoGridItem
                     id={item.id}
                     key={item.id}
